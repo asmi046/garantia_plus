16ab962db65b3f3c402bf271ea4932d345824998
asmi046 <asmi046@gmail.com>
2023-10-02 10:33:55 +0300

    ссылки

--- _theme/template-parts/services.php
+++ _theme/template-parts/services.php
@@ -5,38 +5,32 @@
 
 					<div class="services__inner">
 
-						<div class="services__column">
+						<a href="<?echo get_permalink(203);?>" class="services__column">
 							<img src="<?php echo get_template_directory_uri();?>/img/services/01.webp" alt="Услуга - Обучение">
 							<div class="nuar_blk"></div>
 							<h3 class="services__card-title">Обучение</h3>
 							
-						</div>
+						</a>
 						
-						<div class="services__column">
+						<a href="<?echo get_permalink(205);?>" class="services__column">
 							<img src="<?php echo get_template_directory_uri();?>/img/services/02.webp" alt="Услуга - Периодическая проверка охранников и работников юридических лиц с особыми уставными задачами">
 							<div class="nuar_blk"></div>
 							<h3 class="services__card-title">Периодическая проверка охранников и работников юридических лиц с особыми уставными задачами</h3>
 							
-						</div>
+						</a>
 						
-						<div class="services__column">
+						<a href="<?echo get_permalink(207);?>" class="services__column">
 							<img src="<?php echo get_template_directory_uri();?>/img/services/03.webp" alt="Услуга - Проверка у граждан знания правил безопасного обращения с оружием">
 							<div class="nuar_blk"></div>
 							<h3 class="services__card-title">Проверка у граждан знания правил безопасного обращения с оружием</h3>
 							
-						</div>
+						</a>
 						
-						<div class="services__column">
+						<a href="<?echo get_permalink(209);?>" class="services__column">
 							<img src="<?php echo get_template_directory_uri();?>/img/services/04.webp" alt="Услуга - Учебно-тренировочные стрельбы">
 							<div class="nuar_blk"></div>
-							<h3 class="services__card-title">Учебно-тренировочные стрельбы</h3>
-							
-						</div>
-
-						
-
-						
-						
+							<h3 class="services__card-title">Учебно-тренировочные стрельбы</h3>	
+						</a>
 
 					</div>
 
